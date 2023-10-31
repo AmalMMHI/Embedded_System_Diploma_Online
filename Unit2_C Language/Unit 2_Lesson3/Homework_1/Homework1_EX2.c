0b1c7f528b48dae16dcb587b5da8cd0645580171
AmalMMHI <105753717+AmalMMHI@users.noreply.github.com>
2023-10-31 18:43:56 +0200

    Delete Unit2_C Language directory

--- Unit2_C Language/Unit 2_Lesson3/Homework_1/Homework1_EX2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(void){
-    int x=0;
-    printf("Enter an integer\n");
-    scanf("%d", &x);
-    printf("you entered %d", x);
-
-}
--- Unit2_C Language/Unit 2_Lesson3/Homework_1/Homework1_EX5.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(void){
-    char ch;
-    printf("Enter a character\n");
-    scanf("%c", &ch);
-    printf("the asci value of ch = %d", ch);
-    
-}
